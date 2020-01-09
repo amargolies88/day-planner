b89f8f9eac6373f4357280c91b88eefdb36b066c
Alan Margolies <amargolies88@gmail.com>
2020-01-09 15:42:42 -0500

    refactored js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,10 @@ var topDayDisplay = $("#currentDay");
 
 var timeblockContainer = $("#timeblock-container");
 
-
-
 // Appending timeblocks according to standard work hours
 for (let i = 9; i != 6; i++) {
     if (i >= 13) {i = i - 12;}
-    var timeblockRow = $("<div>").addClass("row row-"+i);
+    var timeblockRow = $("<div>").addClass("row no-gutters row-"+i);
     var timeblockHour = $("<div>").addClass("col-1 hour hour-"+i);
     var timeblock = $("<div>").addClass("col-10 timeblock timeblock-"+i);
     var timeblockSaveBtnCol = $("<div>").addClass("col-1 save-btn-col save-btn-col-"+i);
