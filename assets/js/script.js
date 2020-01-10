c0e09346522056130f771a2751c94f9e7223045a
Alan Margolies <amargolies88@gmail.com>
2020-01-09 21:51:17 -0500

    day updates based on moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,11 @@ var timeblockContainer = $("#timeblock-container");
 
 // Appending timeblocks according to standard work hours
 for (let i = 9; i != 6; i++) {
-    if (i >= 13) {i = i - 12;}
+    var ampm = "AM";
+    if (i >= 13) {
+        ampm = "PM";
+        i = i - 12;
+    }
     var timeblockRow = $("<div>").addClass("row no-gutters row-"+i);
     var timeblockHour = $("<div>").addClass("col-1 hour hour-"+i);
     var timeblock = $("<div>").addClass("col-10 timeblock timeblock-"+i);
@@ -18,7 +22,13 @@ for (let i = 9; i != 6; i++) {
     //Append content to columns
     timeblockSaveBtnCol.append(timeblockSaveBtn);
     timeblock.append(timeblockTextArea);
-    timeblockHour.text(`${i} p.m.`)
+    
+    if (i >= 10){
+        timeblockHour.text(`${i} ${ampm}`)
+    } else{
+        timeblockHour.text(` ${i} ${ampm}`)
+    }
+    
 
     //Append columns to row
     timeblockRow.append(timeblockHour);
@@ -29,3 +39,5 @@ for (let i = 9; i != 6; i++) {
     timeblockContainer.append(timeblockRow);
     console.log("appended timeblock");
 }
+
+topDayDisplay.text(moment().format('dddd'));
