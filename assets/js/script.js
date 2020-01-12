48144c892e490db6aaf7f8a9ae3c41e40113213c
Alan Margolies <amargolies88@gmail.com>
2020-01-11 20:20:05 -0500

    began adding saving functionality (incomplete)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,41 @@
-var topDayDisplay = $("#currentDay");
-
+var topDayDisplay = $("#current-day");
+var topTimeDisplay = $("#current-time");
+var history;
 var timeblockContainer = $("#timeblock-container");
+if (JSON.parse(localStorage.getItem("history")) !== null){
+    history = JSON.parse(localStorage.getItem("history"));
+} else {
+    history = {};
+}
 
-currentHour = parseInt(moment().format("H"));
+function saveHistory(){
+    localStorage.setItem("history", JSON.stringify(history));
+}
 
+//Sets topDayDisplay to show current day and time
+function updateTopDisplays(){
+    topDayDisplay.text(moment().format("dddd"));
+    topTimeDisplay.text(moment().format("h:mm:ss a"));
+}
+
+function updateColor() {
+    for (let i = 9; i < 13 && i < 18; i++) {
+        $(".timeblock-" + i).removeClass("present future");
+        $(".timeblock-" + i).addClass("past");
+    }
+    $(".timeblock-" + 13).removeClass("past future");
+    $(".timeblock-" + 13).addClass("present");
+}
+
+function updateTime(){
+    updateTopDisplays();
+    updateColor();
+}
+
+function saveInput() {
+history[moment().format("yyyy")][moment().format("mm")][moment().format("d")][$(this).attr("data-hour")] = "something";
+console.log(history[moment().format("yyyy")][moment().format("mm")][moment().format("d")][$(this).attr("data-hour")]);
+}
 // Appending timeblocks according to standard work hours
 //Counting loop in military time for simplicity (will change back to regular time before displaying)
 for (let i = 9; i < 18; i++) {
@@ -13,12 +45,12 @@ for (let i = 9; i < 18; i++) {
     } else {
         var ampm = "AM";
     }
-    var timeblockRow = $("<div>").addClass("row no-gutters");
-    var timeblockHour = $("<div>").addClass("col-1 hour");
-    var timeblock = $("<div>").addClass("col-10 timeblock future timeblock-"+i);
-    var timeblockSaveBtnCol = $("<div>").addClass("col-1 save-btn-col");
-    var timeblockTextArea = $("<textarea>").addClass("description");
-    var timeblockSaveBtn = $("<button>").addClass("saveBtn");
+    var timeblockRow = $("<div>").addClass("row no-gutters").attr("data-hour", i);
+    var timeblockHour = $("<div>").addClass("col-2 col-md-1 hour").attr("data-hour", i);
+    var timeblock = $("<div>").addClass("col-8 col-md-10 timeblock future timeblock-" + i).attr("data-hour", i);
+    var timeblockSaveBtnCol = $("<div>").addClass("col-2 col-md-1 save-btn-col").attr("data-hour", i);
+    var timeblockTextArea = $("<textarea>").addClass("description").attr("data-hour", i);
+    var timeblockSaveBtn = $("<button>").addClass("saveBtn btn").attr("data-hour", i);
 
     timeblockRow.attr("data-hour", i);
 
@@ -46,18 +78,12 @@ for (let i = 9; i < 18; i++) {
     console.log("appended timeblock");
 }
 
-//Sets topDayDisplay to show current day
-topDayDisplay.text(moment().format('dddd'));
 
 
 
-function updateColor() {
-    for (let i = 9; i < parseInt(moment().format("H")) && i < 18; i++) {
-        $(".timeblock-"+i).removeClass("present future");
-        $(".timeblock-"+i).addClass("past");
-    }
-    $(".timeblock-"+ parseInt(moment().format("H"))).removeClass("past future");
-    $(".timeblock-"+ parseInt(moment().format("H"))).addClass("present");
+updateTime();
+
+setInterval(function(){
+    updateTime()
 }
-
-updateColor();
+,1000);
